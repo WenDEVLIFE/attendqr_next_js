bebf583bdb9aa27c86fe6871bdcd48fe1d747a2b
WenDEVLIFE <frouenmedinajr@gmail.com>
2026-03-20 13:17:58 +0800

    added all the routes here

--- src/app/admin/route.ts
+++ src/app/admin/route.ts
@@ -2,6 +2,6 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const url = request.nextUrl.clone();
-  url.pathname = '/admin/login';
+  url.pathname = '/auth/login';
   return NextResponse.redirect(url);
 }
