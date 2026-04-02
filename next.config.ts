4a7a1d197c0481e5440c0dc8b9668cf0933829f7
WenDEVLIFE <frouenmedinajr@gmail.com>
2026-04-02 15:28:57 +0800

    fix the next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.0.26.240', '192.168.1.3'],
+  allowedDevOrigins: [
+    'localhost',
+    'localhost:3000',
+    '10.0.26.142',
+    '10.0.26.142:3000',
+    '10.0.26.240',
+    '10.0.26.240:3000',
+    '192.168.1.3',
+    '192.168.1.3:3000',
+  ],
   // @ts-ignore
   turbopack: {
     root: process.cwd(),
